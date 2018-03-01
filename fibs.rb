e3b931023a734436f447a3a64041a4409ca19f2d
Devin Mork <devin.mork@gmail.com>
2018-03-01 07:31:29 -0600

    Add recursive fibonacci sequence.

--- fibs.rb
+++ fibs.rb
@@ -5,16 +5,21 @@
 def fibs(num)
     # Uses loop to return fibonacci sequence up to num
 
-    result = [1]
-    while result.length < num
-        if result.length < 2
-            result << result[-1] + 1
-        else
+    result = [1,1]
+    while result.length <= num
             result << result[-1] + result[-2]
-        end
     end
     result.join(", ")
+end
+
+def fibs_rec(ary, num)
+    # Uses recursion to return fibonacci sequence up to num
+
+    return [1,1] if num == 1
+    ary = fibs_rec(ary, num - 1)
+    ary << ary[-1] + ary[-2]
 
 end
 
 puts fibs(20)
+puts fibs_rec([], 20).join(", ")
