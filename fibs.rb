84d3d9ea5b223ef5889259e49af638a726d2f79b
Devin Mork <devin.mork@gmail.com>
2018-03-01 07:16:42 -0600

    Create fibonacci function with while loop.

--- fibs.rb
+++ fibs.rb
@@ -2,3 +2,19 @@
 # Goal of assignment is to write looping and recursive functions
 # that return the fibonacci sequence up to a given value.
 
+def fibs(num)
+    # Uses loop to return fibonacci sequence up to num
+
+    result = [1]
+    while result.length < num
+        if result.length < 2
+            result << result[-1] + 1
+        else
+            result << result[-1] + result[-2]
+        end
+    end
+    result.join(", ")
+
+end
+
+puts fibs(20)
