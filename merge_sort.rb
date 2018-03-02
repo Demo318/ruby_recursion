4b7c51f76f347cd1dc421f3ab10db05adfc4ebd8
Devin Mork <devin.mork@gmail.com>
2018-03-02 06:38:35 -0600

    Removed puts debugging lines.

--- merge_sort.rb
+++ merge_sort.rb
@@ -8,29 +8,21 @@ def merge_sort(arr)
 
     arr_1 = merge_sort(arr[0..arr.length/2-1])
     arr_2 = merge_sort(arr[arr.length/2..-1])
-    puts "arr_1 #{arr_1} arr_2 #{arr_2}"
-
     new_arr = []
     
     arr_1.each do |i|
         arr_2.each_with_index do |j, j_ind|
             if i < j
-                puts "i<j"
                 break
             elsif j < i
-                puts "j<i"
                 new_arr.append(arr_2.delete_at(j_ind))
-                puts "appended j"
             end
         end
         new_arr.append(i)
-        puts "appended i"
     end
 
     arr_2.each { |j| new_arr.append(j) } unless arr_2.length == 0 
 
-
-    puts "new arr #{new_arr}"
     new_arr
 end
 
