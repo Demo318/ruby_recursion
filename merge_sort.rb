bd08a7b6b1111ae5c10a9cf10d4dac209ca833d1
Devin Mork <devin.mork@gmail.com>
2018-03-02 07:08:27 -0600

    Remove puts debugging.

--- merge_sort.rb
+++ merge_sort.rb
@@ -8,9 +8,8 @@ def merge_sort(arr)
 
     arr_1 = merge_sort(arr[0..arr.length/2-1])
     arr_2 = merge_sort(arr[arr.length/2..-1])
-    puts "arr_1 #{arr_1} arr_2 #{arr_2}"
-    new_arr = []
     
+    new_arr = []
   
     arr_1.each do |i|
         to_delete = 0
@@ -26,12 +25,9 @@ def merge_sort(arr)
         new_arr.append(i)
         end
 
+    arr_2.each { |j| new_arr.append(j) } if arr_2.length > 0
 
-        arr_2.each { |j| new_arr.append(j) } if arr_2.length > 0
-
-    puts "new_arr #{new_arr}"
     new_arr
 end
 
-# p merge_sort([2,4,6,4,3,8,6])
 p merge_sort([0,9,2,12,1,13,8,2,11,7,3,6,15,5,14,4,10])
