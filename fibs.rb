6f667b23a55bffc2a9d11505b1657ff1e3ffcf05
Devin Mork <devin.mork@gmail.com>
2018-03-03 12:58:58 -0600

    Linted a lot.

--- fibs.rb
+++ fibs.rb
@@ -3,22 +3,21 @@
 # that return the fibonacci sequence up to a given value.
 
 def fibs(num)
-    # Uses loop to return fibonacci sequence up to num
+  # Uses loop to return fibonacci sequence up to num
 
-    result = [1,1]
-    while result.length <= num
-            result << result[-1] + result[-2]
-    end
-    result.join(", ")
+  result = [1, 1]
+
+  result << result[-1] + result[-2] while result.length <= num
+  result.join(', ')
 end
 
 def fibs_rec(ary, num)
-    # Uses recursion to return fibonacci sequence up to num
+  # Uses recursion to return fibonacci sequence up to num
 
-    return [1,1] if num == 1
-    ary = fibs_rec(ary, num - 1)
-    ary << ary[-1] + ary[-2]
+  return [1, 1] if num == 1
+  ary = fibs_rec(ary, num - 1)
+  ary << ary[-1] + ary[-2]
 end
 
 puts fibs(20)
-puts fibs_rec([], 20).join(", ")
+puts fibs_rec([], 20).join(', ')
--- merge_sort.rb
+++ merge_sort.rb
@@ -2,32 +2,41 @@
 # Implement merg sort algorithm in ruby function, using recursion.
 
 def merge_sort(arr)
-    # Follows the classic 'merge sort' methodology.
+  # Follows the classic 'merge sort' methodology.
 
-    return arr if arr.length == 1
+  return arr if arr.length == 1
 
-    arr_1 = merge_sort(arr[0..arr.length/2-1])
-    arr_2 = merge_sort(arr[arr.length/2..-1])
-    
-    new_arr = []
-  
-    arr_1.each do |i|
-        to_delete = 0
-        arr_2.each do |j|
-            if j <= i
-                new_arr.append(j)
-                to_delete += 1
-            else # j > i
-                break
-            end
-        end
-        to_delete.times { arr_2.shift }
-        new_arr.append(i)
-        end
+  arr_one = merge_sort(arr[0..arr.length / 2 - 1])
+  arr_two = merge_sort(arr[arr.length / 2..-1])
 
-    arr_2.each { |j| new_arr.append(j) } if arr_2.length > 0
-
-    new_arr
+  cycle_through_arrays(arr_one, arr_two)
 end
 
-p merge_sort([0,9,2,12,1,13,8,2,11,7,3,6,15,5,14,4,10])
+def cycle_through_arrays(arr_one, arr_two)
+  # Moves through both arrays, returns sorted.
+
+  new_arr = []
+
+  arr_one.each do |i|
+    to_delete = 0
+    arr_two.each do |j|
+      break if j > i
+      new_arr.append(j)
+      to_delete += 1
+    end
+    to_delete.times { arr_two.shift }
+    new_arr.append(i)
+  end
+
+  new_arr = append_extras(arr_two, new_arr) unless arr_two.empty?
+  new_arr
+end
+
+def append_extras(arr, new_arr)
+  # Takes leftover values and adds them on.
+
+  arr.each { |j| new_arr.append(j) }
+  new_arr
+end
+
+p merge_sort([0, 9, 2, 12, 1, 13, 8, 2, 11, 7, 3, 6, 15, 5, 14, 4, 10])
